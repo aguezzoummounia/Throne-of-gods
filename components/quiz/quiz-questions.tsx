ccb9a2b0c4d338af80a956ce70d79bf5b617d169
username-soa <younesbenlmouden@gmail.com>
2025-08-07 01:59:59 +0100

    quiz component update 2, adding new questions and answers data, adding indicator component

--- components/quiz/quiz-questions.tsx
+++ components/quiz/quiz-questions.tsx
@@ -1,9 +1,16 @@
 "use client";
+import Text from "../ui/text";
+import { cn } from "@/lib/utils";
 import { useState } from "react";
+import Button from "../ui/button-or-link";
 import { useRouter } from "next/navigation";
 import type { Question, VillainKey } from "@/lib/types";
-import Text from "../ui/text";
-import Button from "../ui/button-or-link";
+
+// TODO: this needs a new button styles
+// make questions and answers 5 words max for consistency
+// handle the case where a question reflects more than just one character
+// add is answered state to indicators
+// change indicator line svg
 
 interface QuizClientProps {
   questions: readonly Question[];
@@ -45,7 +52,7 @@ export function QuizQuestions({ questions }: QuizClientProps) {
 
   return (
     <div className="flex items-center justify-center flex-1 relative">
-      <div className="flex flex-col items-center justify-center gap-10 lg:w-[58.33%] xs:w-[83.33%] w-full">
+      <div className="flex flex-col items-center justify-center gap-10 md:w-[65%] w-full">
         <div className="flex flex-col items-center justify-center gap-2">
           <Text as="h4" variant="xs" className="text-center uppercase">
             Question {currentQuestionIndex + 1}
@@ -54,13 +61,14 @@ export function QuizQuestions({ questions }: QuizClientProps) {
             {currentQuestion.text}
           </Text>
         </div>
-        <div className="flex items-center justify-center flex-wrap md:gap-6 gap-10">
+        <div className="flex items-center justify-center flex-wrap md:gap-6 gap-10 md:max-w-[55rem]">
           {currentQuestion.answers.map((answer, index) => (
             <Button
               animated
               size="loose"
+              key={`response-button-${index}`}
               onClick={() => handleAnswer(answer.villain)}
-              className="text-xs md:w-44 w-[80%] md:h-14 h-14 uppercase"
+              className="text-xs md:w-52 w-[80%] md:h-14 h-14 uppercase"
             >
               {answer.text}
             </Button>
@@ -68,29 +76,185 @@ export function QuizQuestions({ questions }: QuizClientProps) {
         </div>
       </div>
       {/* navigation indicators */}
-      <div className="bg-orange-950 absolute inset-[auto_0%_1rem] flex items-center justify-center">
-        ..........
+      <div className="absolute inset-[auto_0%_1rem] flex items-center justify-center">
+        {questions.map((_, index) => {
+          return (
+            <Indicator
+              number={index + 1}
+              isLast={index + 1 === questions.length}
+              isActive={currentQuestionIndex === index}
+              handleClick={() => setCurrentQuestionIndex(index)}
+            />
+          );
+        })}
       </div>
     </div>
   );
 }
 
-// <form>
-//   <fieldset>
-//     <legend>{currentQuestion.text}</legend>
-//     {currentQuestion.answers.map((answer, index) => (
-//       <div key={index}>
-//         <input
-//           type="radio"
-//           id={`q${currentQuestion.id}-a${index}`}
-//           name={`question-${currentQuestion.id}`}
-//           value={answer.villain}
-//           onChange={() => handleAnswerChange(answer.villain)}
-//         />
-//         <label htmlFor={`q${currentQuestion.id}-a${index}`}>
-//           {answer.text}
-//         </label>
-//       </div>
-//     ))}
-//   </fieldset>
-// </form>
+const Indicator: React.FC<{
+  number: number;
+  isLast?: boolean;
+  isActive: boolean;
+  handleClick: () => void;
+}> = ({ number, isLast, isActive, handleClick }) => {
+  return (
+    <>
+      <button
+        role="button"
+        onClick={handleClick}
+        title={`skip to question ${number}`}
+        className={cn(
+          "cursor-pointer relative block md:w-14 md:h-14 w-10 h-10 transition-opacity opacity-70",
+          isActive && "opacity-100"
+        )}
+      >
+        <span className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex items-center justify-center p-1.5 text-[#8F7E77] font-cinzeldecorative md:text-sm text-xs">
+          {number}
+        </span>
+        <svg
+          fill="none"
+          viewBox="0 0 88 89"
+          xmlns="http://www.w3.org/2000/svg"
+          className={cn(
+            "transition-transform ease-[cubic-bezier(.25,1,.5,1)] hover:scale-110 duration-[.6s] w-full h-full absolute inset-0",
+            `delay-[${number * 0.1}]`
+          )}
+        >
+          <g filter="url(#filter0_f_6587_3779)">
+            <path
+              fill="none"
+              d="M79.2772 39.8839C85.1004 61.603 71.2873 75.883 45.8134 82.7045C26.5499 87.8629 13.5523 68.9115 8.57604 50.3515C3.11311 26.843 21.1028 12.7143 40.3664 7.55583C59.6299 2.3974 72.3952 14.2159 79.2772 39.8839Z"
+              stroke="#8F7E77"
+              className="rotate-svg-animation-reverse"
+            ></path>
+          </g>
+          <path
+            fill="none"
+            className="rotate-svg-animation-reverse"
+            d="M79.6823 40.689C85.4941 62.6302 71.6766 77.0648 46.2038 83.9693C26.9412 89.1905 13.955 70.0491 8.98853 51.2993C3.53783 27.5499 21.5312 13.2661 40.7938 8.04493C60.0564 2.82373 72.8138 14.7584 79.6823 40.689Z"
+            stroke="#8F7E77"
+          ></path>
+          <path
+            fill="none"
+            opacity="0.4"
+            className="rotate-svg-animation"
+            d="M62.4666 15.6093C84.6472 25.7798 87.7796 45.5223 77.024 68.9216C68.8907 86.6163 44.3981 83.6003 25.4438 74.9092C1.98109 63.2791 1.68789 40.5466 9.82126 22.8519C17.9546 5.1572 36.2532 3.58968 62.4666 15.6093Z"
+            stroke="#8F7E77"
+          ></path>
+          <defs>
+            <filter
+              id="filter0_f_6587_3779"
+              x="3.08325"
+              y="1.86932"
+              width="82.0162"
+              height="86.196"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="BackgroundImageFix"
+                result="shape"
+              ></feBlend>
+              <feGaussianBlur
+                stdDeviation="2"
+                result="effect1_foregroundBlur_6587_3779"
+              ></feGaussianBlur>
+            </filter>
+          </defs>
+        </svg>
+      </button>
+      {!isLast && (
+        <div className="h-[5px] max-md:w-[30px] flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            viewBox="0 0 165 9"
+            fill="none"
+            class="quiz-line is-inactive"
+          >
+            <path
+              d="M10.6765 4.64002H6.67651V4.13195H10.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M15.6765 4.64002H18.6765V4.13195H15.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M27.6765 4.64002H21.6765V4.13195H27.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M35.6765 4.64002H44.6765V4.13195H35.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M53.6765 4.64002H47.6765V4.13195H53.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M56.6765 4.64002H62.6765V4.13195H56.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M76.6765 4.64002H69.6765V4.13195H76.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M79.6765 4.64002H82.6765V4.13195H79.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M86.6765 4.64002H83.6765V4.13195H86.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M87.6765 4.64002H91.6765V4.13195H87.6765V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M102.677 4.64002H97.6765V4.13195H102.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M104.677 4.64002H109.677V4.13195H104.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M119.677 4.64002H114.677V4.13196H119.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M122.677 4.64002H128.677V4.13196H122.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M135.677 4.64002H131.677V4.13196H135.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M141.677 4.64002H146.677V4.13196H141.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M156.677 4.64002H149.677V4.13196H156.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M160.677 4.64002H164.422V9.00024H164.931V0.000244141H164.422V4.13196H160.677V4.64002Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M4.67651 4.13195V4.64002H0.676514V4.13195H4.67651Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      )}
+    </>
+  );
+};
--- lib/data.ts
+++ lib/data.ts
@@ -2,68 +2,235 @@ const quizData = {
   questions: [
     {
       id: 1,
-      text: "What is your ideal Friday night?",
+      text: "When power slips from your grasp, how do you reclaim it?",
       answers: [
         {
-          text: "Plotting world domination from my secret lair.",
-          villain: "magneto",
+          text: "Outwit those above me",
+          villain: "leticia",
+          // Leticia, Isolde
         },
         {
-          text: "Causing a little bit of chaos, just for fun.",
-          villain: "joker",
+          text: "Burn the world if I must",
+          villain: "eternal-flames",
+          // Agon, Order of Eternal Flames
         },
         {
-          text: "Working on a complex algorithm to optimize my finances.",
-          villain: "lex-luthor",
+          text: "Wait in the shadows. The deeper the patience, the sharper the strike",
+          villain: "harald",
+          // Queen of the Deep, Harald
         },
         {
-          text: "Ensuring the universe is perfectly balanced.",
-          villain: "thanos",
+          text: "Declare myself chosen. Those who disagree are unworthy",
+          villain: "agon",
+          // Jarvan I, Agon
+        },
+        {
+          text: "Crush the weak and rebuild my empire from their bones",
+          villain: "valeon",
+          // Nicolas, Valeon
         },
       ],
     },
     {
       id: 2,
-      text: "What is your primary motivation?",
+      text: "What would you sacrifice for eternal control?",
       answers: [
         {
-          text: "Protecting my people, by any means necessary.",
-          villain: "magneto",
+          text: "Love. It’s a weakness I buried long ago",
+          villain: "isolde",
+          // Isolde, Queen of the Deep
         },
-        { text: "To prove that anyone can be broken.", villain: "joker" },
-        { text: "Unimaginable power and influence.", villain: "lex-luthor" },
-        { text: "Bringing order to a chaotic universe.", villain: "thanos" },
+        {
+          text: "The lives of others",
+          villain: "agon",
+          // Agon, Order of Eternal Flames
+        },
+        {
+          text: "My name, my past, even my soul. Only power matters",
+          villain: "harald",
+          // Valeon, Harald
+        },
+        {
+          text: "Nothing. Control is my birthright, not a bargain",
+          villain: "jarvan",
+        },
+        {
+          text: " Everything. Especially those who ever doubted me",
+          villain: "leticia",
+          // Leticia, Nicolas
+        },
+      ],
+    },
+    {
+      id: 3,
+      text: "When confronted with betrayal, how do you respond?",
+      answers: [
+        {
+          text: "I feign ignorance… until the knife turns in their gut",
+          villain: "isolde",
+          // Leticia, Isolde
+        },
+        {
+          text: "I declare holy vengeance, with flames for justice",
+          villain: "eternal-flames",
+          // Agon, Order of Eternal Flames
+        },
+        {
+          text: "I disappear. But they drown in nightmares of my return",
+          villain: "queen-of-deep",
+        },
+        {
+          text: "I strike first. Let none dare betray me again",
+          villain: "jarvan",
+        },
+        {
+          text: "I make them beg for forgiveness… and then flay their name from history",
+          villain: "nicolas",
+          //Nicolas, Valeon
+        },
+      ],
+    },
+    {
+      id: 4,
+      text: "What is your legacy, carved in blood and history",
+      answers: [
+        {
+          text: "A kingdom built from shadows, puppets, and lies",
+          villain: "leticia",
+          // Isolde, Leticia
+        },
+        {
+          text: "A war cry that still echoes through burning altars",
+          villain: "agon",
+          // Agon, Order of Eternal Flames
+        },
+        {
+          text: "A name erased by gods… and returned through vengeance",
+          villain: "queen-of-deep",
+          // Valeon, Harald
+        },
+        {
+          text: "The first to silence heaven. The last to kneel",
+          villain: "jarvan",
+        },
+        {
+          text: "None will speak of me kindly. But they will remember",
+          villain: "leticia",
+          // Nicolas, Harald, Valeon
+        },
+      ],
+    },
+    {
+      id: 5,
+      text: "Which quote resonates with your soul the most?",
+      answers: [
+        {
+          text: "The weak pray. The strong rule",
+          villain: "jarvan",
+        },
+        { text: "Gold burns brighter than glory.", villain: "valeon" },
+        { text: "Power is beauty. Beauty is control.", villain: "leticia" },
+        {
+          text: "Let the world believe my pain if it keeps me on the throne.",
+          villain: "isolde",
+        },
+        {
+          text: "We rose from darkness to reclaim the sun.",
+          villain: "king-of-deep",
+        },
+      ],
+    },
+    {
+      id: 6,
+      text: "In the end, what do you truly seek?",
+      answers: [
+        {
+          text: "Immortality through legacy and wealth.",
+          villain: "valeon",
+        },
+        { text: "Control everything from the shadows.", villain: "leticia" },
+        {
+          text: "To stay in charge, no matter who I have to hurt",
+          villain: "isolde",
+        },
+        {
+          text: "To break all rules and become more powerful than the gods",
+          villain: "jarvan",
+        },
+        { text: "To feel strong by keeping others weak", villain: "nicolas" },
       ],
     },
   ],
   villains: {
-    joker: {
-      name: "The Joker",
-      slug: "joker",
+    jarvan: {
+      name: "Jarvan the First",
+      slug: "jarvan",
       image: "/images/villains/joker.jpg",
       description:
-        "You are an agent of chaos. You don't have a grand plan; you just want to watch the world burn. Your unpredictability is your greatest strength.",
+        "He ruled Galeeria and corrupted divine power. He outlawed prayer and burned temples, declaring, “The strong rule,” and “The weak pray”. His actions contributed to the world's cracking through clashing powers",
     },
-    magneto: {
-      name: "Magneto",
-      slug: "magneto",
+    valeon: {
+      name: "Valeon",
+      slug: "valeon",
       image: "/images/villains/magneto.jpg",
       description:
-        "You are a righteous revolutionary. You fight for a cause you believe in, but your methods are extreme. You are powerful, charismatic, and unwavering.",
+        "He turned his sacred flame into currency, churning steel for any king with gold, and profited from the war between Jarvan and Crascius. He is mentioned to have died from a blood curse that now plagues his lineage, which is believed to be the same affliction that impacted Reynold and Queen Isolde's son",
     },
-    thanos: {
-      name: "Thanos",
-      slug: "thanos",
+    leticia: {
+      name: "Empress Leticia",
+      slug: "leticia",
       image: "/images/villains/thanos.jpg",
       description:
-        "You are a righteous revolutionary. You fight for a cause you believe in, but your methods are extreme. You are powerful, charismatic, and unwavering.",
+        "She was so beautiful that Jarvan XI fell for her, but she craved power after giving him three sons. She is portrayed as ruthless and manipulative, orchestrating a plot to frame Kaen for Emperor Jarvan XI's murder and then attempting to have him killed at the funeral. She despises Kaen and vows to make his existence 'ash and memory'. Kaen views her as a 'masterpiece of cruelty waltzing hand-in-hand with doom'",
     },
-    "lex-luthor": {
-      name: "Lex Luthor",
-      slug: "lex-luthor",
+    agon: {
+      name: "Prince Agon",
+      slug: "agon",
       image: "/images/villains/lex-luthor.jpg",
       description:
-        "You are a righteous revolutionary. You fight for a cause you believe in, but your methods are extreme. You are powerful, charismatic, and unwavering.",
+        "He was arrogant and believed himself divinely chosen, with the court and clergy supporting his claim to the throne. He accused Kaen of patricide and, despite Kaen's claims of innocence, delivered the killing blow to Kaen at the funeral. He was defeated by Kaen's sudden awakening of divine lightning during the Crown Prince Tournament",
+    },
+    isolde: {
+      name: "Queen Isolde Thalakar",
+      slug: "isolde",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "She is described as ruthless, calculating, and impossibly wealthy. She married into power and refuses to yield authority to her son, even though he is an adult. She believes only in power and manipulates public perception, notably by physically harming her own son, Teralion, to create the illusion that he suffers from the Valeon blood curse and thus affirming her divine right to rule. She abducted Sir Anthony to prevent him from revealing truths and declared the Duke's family traitors. She shows no remorse for her actions, including letting her husband die",
+    },
+    "eternal-flames": {
+      name: "The Order of Eternal Flames",
+      slug: "eternal-flames",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "They preach purity and fire and gained significant influence, even reaching Galeeria's throne. They declared Agon the 'true heir chosen by the gods'. Kaen and Jarvan XI aim to 'wipe' them from the world. Kaen intends to strip them of their power, wealth, and influence, including burning their temples",
+    },
+    nicolas: {
+      name: "Nicolas",
+      slug: "nicolas",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "He was arrogant and believed himself divinely chosen, with the court and clergy supporting his claim to the throne. He accused Kaen of patricide and, despite Kaen's claims of innocence, delivered the killing blow to Kaen at the funeral. He was defeated by Kaen's sudden awakening of divine lightning during the Crown Prince Tournament",
+    },
+    harald: {
+      name: "Harald",
+      slug: "harald",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "He was another overseer who tried to seize Kaen after Nicolas's death and initially believed Kaen to be possessed by a devil. He is later revealed to be a trained soldier in a covert military operation",
+    },
+    "king-of-deep": {
+      name: "King of the deep",
+      slug: "king-of-deep",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "They served as the primary antagonists in the War of Depths, leading monstrous creatures that crawled up from the deep and caused widespread devastation. They represented unity, purpose, and love, qualities that the three emperors lacked. Both turned to ash and rode the wind away after Goddess Law commanded them to return to the depths",
+    },
+    "queen-of-deep": {
+      name: "Queen of the deep",
+      slug: "king-of-Queen",
+      image: "/images/villains/lex-luthor.jpg",
+      description:
+        "They served as the primary antagonists in the War of Depths, leading monstrous creatures that crawled up from the deep and caused widespread devastation. They represented unity, purpose, and love, qualities that the three emperors lacked. Both turned to ash and rode the wind away after Goddess Law commanded them to return to the depths",
     },
   },
 } as const;
