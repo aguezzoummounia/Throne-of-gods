3696192b922e57bfccb41004eec815dc353e0aa6
username-soa <younesbenlmouden@gmail.com>
2025-09-12 16:04:35 +0100

    updating SvgOutline svg lines into new style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-"use client";
 import Map from "@/components/map";
 import Quiz from "@/components/quiz";
 import Hero from "@/components/hero/hero";
 import About from "@/components/about/about";
+import Footer from "@/components/global/footer";
 import CharacterSlider from "@/components/slider/character-slider";
 import HomeScrollContainer from "@/components/home-scroll-container";
-import Footer from "@/components/global/footer";
 // import ShaderCanvas from "@/components/test-shape";
 
 export default function Home() {
