56214674812bd8488b5b643f7e5e036c142e4761
username-soa <younesbenlmouden@gmail.com>
2025-09-14 21:09:13 +0100

    improving the animateout function for page transition

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <Hero />
         <About />
         <Map />
-        <CharacterSlider />
+        {/* <CharacterSlider /> */}
         <Quiz />
       </HomeScrollContainer>
       <Footer />
